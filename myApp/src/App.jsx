d5e27f583b0a690e5b84472447a8ae565714fb2b
avishkar_1465 <avishkarparadkar@gmail.com>
2025-03-20 14:45:20 +0530

    update

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -5,14 +5,12 @@ import Workflow from "./components/Workflow";
 import Footer from "./components/Footer";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
-import Watch from "./components/Watch";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
-        <Watch />
         <HeroSection />
         <FeatureSection />
         <Workflow />
